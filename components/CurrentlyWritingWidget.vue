a04ec3ad64a71f764f348160882cbadd21b13a3e
Chris Gardiner-Bill <cbill.uk@gmail.com>
2024-11-05 18:09:16 +1100

    adds links to writing widget

--- components/CurrentlyWritingWidget.vue
+++ components/CurrentlyWritingWidget.vue
@@ -3,8 +3,10 @@
 <div v-if="data" class="surface-card p-6 shadow-2 border-round border-2 border-surface surface-border">
     <div class="text-3xl md:text-4xl font-bold mb-4">What I'm writing</div>
     <div v-for="book in getWritingNow(data)" class="mb-4">
-        <div class="font-semibold text-lg mt-0 mb-0">{{ book.title }}</div>
-        <progress class="progress w-full" :value="book.progress" max="100"></progress>
+        <NuxtLink :to="book.url">
+            <div class="font-semibold text-lg mt-0 mb-0">{{ book.title }}</div>
+            <progress class="progress w-full" :value="book.progress" max="100"></progress>
+        </NuxtLink>
     </div>
 </div>
 
