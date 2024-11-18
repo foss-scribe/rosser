39d99e6081989a234d19688d055ec0bd2af5c0ea
Chris Gardiner-Bill <cbill.uk@gmail.com>
2024-11-18 21:41:06 +1100

    adds glossary template

---
+++ pages/worlds/alashiya/glossary.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div class="text-center my-6 relative">
+        <div class="text-6xl font-bold mb-1 heading">{{ data.title }}</div>
+        <p class="mt-4 mb-3 line-height-3 text-center mx-auto text-3xl" style="max-width:500px">{{ data.description }}</p>
+    </div>
+
+    <div class="px-2 md:px-28">
+        <div v-for="term in data.terms" class="collapse collapse-arrow bg-base-200 my-3">
+            <input type="radio" name="my-accordion-2" />
+            <div class="collapse-title text-xl font-medium">{{ term.term }}</div>
+            <div class="collapse-content">
+                {{ term.description }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const data = await queryContent('/worlds/alashiya/glossary').findOne()
+
+</script>
