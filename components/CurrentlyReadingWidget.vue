219fd4978fa800145d5e9f50c34bf81a0b235332
Chris Gardiner-Bill <cbill.uk@gmail.com>
2024-11-22 12:48:47 +1100

    updates reading now widget

--- components/CurrentlyReadingWidget.vue
+++ components/CurrentlyReadingWidget.vue
@@ -1,19 +1,25 @@
 <template>
 
 <NuxtLink to="/reading">
-    <div v-if="data" class="surface-card p-6 shadow-2 border-round border-2 border-surface surface-border">
+    <div class="surface-card p-6 shadow-2 border-round border-2 border-surface surface-border">
         <div class="text-3xl md:text-4xl font-bold mb-4">What I'm reading</div>
-        <div v-for="book in getReadingNow(data)" class="mb-4">
-            <div class="font-semibold text-lg mt-0 mb-0">{{ book.title }}<br />by {{ book.author }}</div>
-            <progress class="progress w-full" :value="book.progress" max="100"></progress>
+        <div v-if="books && books.length > 0">
+            <div v-for="book in books" class="mb-4">
+                <div class="font-semibold text-lg mt-0 mb-0">{{ book.title }}<br />by {{ book.author }}</div>
+                <progress class="progress w-full" :value="book.progress" max="100"></progress>
+            </div>
+        </div>
+        <div v-else>
+            Nothing's on my current reading list.
         </div>
     </div>
 </NuxtLink>
-
 </template>
 
 <script setup>
 const { data } = await useAsyncData(() => queryContent('/reading_list').findOne())
+
+const books = ref()
 
 function getReadingNow(data) {
     return data.books.filter((book) => {
@@ -21,4 +27,10 @@ function getReadingNow(data) {
     })
 }
 
+onMounted(() => {
+
+        console.log(data.value)
+        books.value = getReadingNow(data.value)
+
+})
 </script>
