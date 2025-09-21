bf6ba400d50c7d7bf331ae40ae1b7796b7c2c972
Chris Gardiner-Bill <cbill.uk@gmail.com>
2025-09-21 16:10:27 +1000

    updates dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,6 +42,5 @@ export default defineNuxtConfig({
     '/posts/2018/08/11/review-omnioutliner/': { redirect: 'https://chrisrosser.medium.com/omnioutliner-review-68bd3b1e5735'},
     '/posts/2021/04/27/how-to-create-a-parchment-background-in-procreate/' : { redirect: 'https://chrisrosser.medium.com/how-to-create-a-parchment-background-in-procreate-f1517b129215'},
     '/posts/2020/04/15/how-to-create-a-paperback-with-apple-pages/': { redirect: 'https://chrisrosser.substack.com/p/how-to-create-a-paperback-with-apple'}
-  },
-  nitro: { preset: 'cloudflare_pages' }
+  }
 })
