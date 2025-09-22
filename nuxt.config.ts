c275c1674902139c96fcc139c27539e783faac45
Chris Gardiner-Bill <cbill.uk@gmail.com>
2025-09-22 15:21:05 +1000

    adds redirect route rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,10 +20,10 @@ export default defineNuxtConfig({
   umami: {
     id: "a235a069-e115-4729-b868-a38dd310ee95",
     host: "https://my-umami.xyz", 
+  },
+  routeRules: {
+    '/posts/2018/08/11/review-omnioutliner/': { redirect: 'https://chrisrosser.medium.com/omnioutliner-review-68bd3b1e5735' },
+    '/posts/2021/04/27/how-to-create-a-parchment-background-in-procreate/': { redirect: 'https://chrisrosser.medium.com/how-to-create-a-parchment-background-in-procreate-f1517b129215' },
+    '/posts/2020/04/15/how-to-create-a-paperback-with-apple-pages/': { redirect: 'https://chrisrosser.substack.com/p/how-to-create-a-paperback-with-apple' }
   }
-  // routeRules: {
-  //   '/posts/2018/08/11/review-omnioutliner/': { redirect: 'https://chrisrosser.medium.com/omnioutliner-review-68bd3b1e5735' },
-  //   '/posts/2021/04/27/how-to-create-a-parchment-background-in-procreate/': { redirect: 'https://chrisrosser.medium.com/how-to-create-a-parchment-background-in-procreate-f1517b129215' },
-  //   '/posts/2020/04/15/how-to-create-a-paperback-with-apple-pages/': { redirect: 'https://chrisrosser.substack.com/p/how-to-create-a-paperback-with-apple' }
-  // }
 })
