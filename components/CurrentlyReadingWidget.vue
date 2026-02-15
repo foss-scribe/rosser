d783c0399be77c218061cb3a4e6db9a9f1c6a997
Chris Gardiner-Bill <cbill.uk@gmail.com>
2026-02-15 16:21:18 +1100

    Replaces local JSON reading list with Pocketbase

--- components/CurrentlyReadingWidget.vue
+++ components/CurrentlyReadingWidget.vue
@@ -3,9 +3,9 @@
 <NuxtLink to="/reading">
     <div class="surface-card p-6 shadow-2 border-round border-2 rounded-md">
         <div class="text-3xl md:text-4xl font-bold mb-4">What I'm reading</div>
-        <div v-if="readingList && readingList.meta.books.length > 0">
-            <div v-for="book in getReadingNow(readingList.meta.books)" class="mb-4">
-                <div class="font-semibold text-lg mt-0 mb-0">{{ book.title }}<br />by {{ book.author }}</div>
+        <div v-if="readingList && readingList.items.length > 0">
+            <div v-for="book in readingList.items" class="mb-4">
+                <div class="font-semibold text-lg mt-0 mb-0">{{ book.title }}<br />by {{ book.expand.authors[0].firstName }} {{ book.expand.authors[0].lastName }}</div>
                 <progress class="progress w-full" :value="book.progress" max="100"></progress>
             </div>
         </div>
@@ -17,18 +17,14 @@
 </template>
 
 <script setup>
-const { data: readingList } = await useAsyncData('readingList', () => {
-  return queryCollection('readingList')
-    .order('title', 'ASC')
-    .first()
-})
+import PocketBase from 'pocketbase';
 
+const pb = new PocketBase('https://pb-crnet.de1.53675094.xyz');
 
+const readingList = await pb.collection('books').getList(1, 999,{
+    expand: 'authors',
+    filter: 'status="reading"'
+});
 
-function getReadingNow(books) {
-    return books.filter((book) => {
-        return book.status === 'currently-reading';
-    })
-}
 
 </script>
